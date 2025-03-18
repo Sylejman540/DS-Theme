4b85997ffb110e61b6cc7caff30da6d77f664739
SylejmanDurguti <durgutisylejman00@gmail.com>
2025-03-18 18:53:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,38 @@
 <?php get_header(); ?>
 
-<main>
-    <h1>Welcome to my First Theme</h1>
-    <?php 
-    if(have_posts()) :
-        while(have_posts()) : the_post();
-            the_title('<h2>', '</h2>');
-            the_content();
-        endwhile;
-    else:
-        echo '<p> No Content Available</p>';
-    endif;
-    ?>
-</main>
+<!-- Hero Section -->
+<div id="content" class="site-content">
+    <div id="primary" class="content-area">
+        <main id="main" class="side-main">
+            <section class="services">
+                Made By Sylejman
+            </section>
+
+            <section class="home-blog">
+                <div class="container">
+                    <div class="blog-items">
+                        <?php 
+                        if ( have_posts()) : the_post();
+                            while ( have_posts() ) : 
+                                the_post();
+                        ?>
+                            <article>
+                                <h2><?php the_title(); ?></h2>
+                                <div class="meta-info">
+                                    <p>Posted in by <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
+                                    <p>Categories: <?php the_category();?></p>
+                                    <p>Tags: <?php the_tags(); ?></p>
+                                </div>
+                                <?php the_content(); ?>
+                            </article>
+                        <?php 
+                            endwhile;
+                        endif;?>
+                    </div>
+                </div>
+            </section>
+        </main>
+    </div>
+</div>
 
 <?php get_footer(); ?>
